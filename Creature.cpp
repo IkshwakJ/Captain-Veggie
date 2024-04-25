e5aa9e8a2b803cab45455f7f2b7fae2b703519da
IkshwakJ <ikshwakjinesh@gmail.com>
2024-04-24 23:37:22 -0400

    Create Creature.cpp

---
+++ Creature.cpp
@@ -0,0 +1,34 @@
+#include"Creature.h"
+// @brief Contructor for the creature class
+// @param 1 integer input that indicates the vertical position of the creature.
+// @param 2 integer input that indicates the horizontal position of the creature.
+// @param 3 string input that indicates the symbol of the creature and will be passed to the constructor of FieldInhabitant.
+Creature::Creature(int input_heightpos, int input_widthpos, string input_symbol):FieldInhabintant(input_symbol)
+{
+    heightpos = input_heightpos;
+    widthpos = input_widthpos;
+}
+// @brief getter function to return the vertical position of the creature.
+// @return the value stored in member variable heightpos as integer.
+int Creature::getheightpos()
+{
+    return heightpos;
+}
+// @brief getter function to return the horizontal position of the creature.
+// @return the value stored in member variable widthpos as integer.
+int Creature::getwidthpos()
+{
+    return widthpos;
+}
+// @brief setter function that enters the passed integer into the heightpos member variable.
+// @param integer value that is to be stored in the heightpos member variable.
+void Creature::setheightpos(int input_heightpos)
+{
+    heightpos = input_heightpos;
+}
+// @brief setter function that enters the passed integer into the widthpos member variable.
+// @param integer value that is to be stored in the widthpos member variable.
+void Creature::setwidthpos(int input_widthpos)
+{
+    widthpos = input_widthpos;
+}
