150943d2b691749513700157c5d8e1e526ca76a4
IkshwakJ <ikshwakjinesh@gmail.com>
2024-04-24 22:35:28 -0400

    Update FieldInhabitant.cpp
    Added Comments

--- FieldInhabitant.cpp
+++ FieldInhabitant.cpp
@@ -1,16 +1,21 @@
 #include"FieldInhabitant.h"
-
+// @brief constructor, enters the passed string to member variable.
+// @param string input.
 FieldInhabintant::FieldInhabintant(string input_symbol)
 {
     symbol = input_symbol;
 }
+// @brief returns the member varable symbol.
+// @return stored value in symbol variable as string.
 string FieldInhabintant::getsymbol()
 {
     return symbol;
 }
+// @brief Updates the member variable using the passed string.
 void FieldInhabintant::setsymbol(string input_symbol)
 {
     symbol = input_symbol;
 }
+// @brief destructor is virtual and has no function.
 FieldInhabintant::~FieldInhabintant()
 {}
