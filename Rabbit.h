4be34ff920cc20aa00a0c2f36b51f96dad072650
ArchieJ98 <162053107+ArchieJ98@users.noreply.github.com>
2024-05-02 21:58:54 -0400

    Update Rabbit.h comments

--- Rabbit.h
+++ Rabbit.h
@@ -1,3 +1,8 @@
+/* Author: Archith J, Ikshwak Jinesh
+Date: 5-2-2024
+Description: Header file to define the Rabbit class and its functions.
+*/
+
 #ifndef Rabbit_h
 #define Rabbit_h
     #include"Creature.h"
