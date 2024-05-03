c964344cfd3c35a242d714eb6ddd51142e01e1ad
ArchieJ98 <162053107+ArchieJ98@users.noreply.github.com>
2024-05-02 22:00:46 -0400

    Update Snake.cpp comments

--- Snake.cpp
+++ Snake.cpp
@@ -1,3 +1,8 @@
+/* Author: Archith J, Ikshwak Jinesh
+Date: 5-2-2024
+Description: Program defining the Snake class functions.
+*/
+
 #include"Snake.h"
 // @brief This constructor function takes in input an pass them to the constructor of the creature class, along with the symbol for the Snake.
 // @param 1 input_heightpos is an integer representing the snakes vertical position on the map.
